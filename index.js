243d1a84a63023371f2f73695df4bfe1341bb5e4
Moses <moseskarunia@gmail.com>
2025-02-13 15:56:21 +0700

    use 2 qtys

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let qty = 0;
+let qty2 = 0;
 
 function addQty() {
     qty = qty + 1;
@@ -8,4 +9,14 @@ function addQty() {
 function substractQty() {
     qty = qty - 1;
     document.getElementById("qty").innerHTML = qty;
+}
+
+function addQty2() {
+    qty2 = qty2 + 1;
+    document.getElementById("qty2").innerHTML = qty2;
+}
+
+function substractQty2() {
+    qty2 = qty2 - 1;
+    document.getElementById("qty2").innerHTML = qty2;
 }
