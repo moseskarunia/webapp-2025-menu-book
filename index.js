0cdd6b07f04e05547fecce24da41f2d84de22311
Moses <moseskarunia@gmail.com>
2025-02-20 15:43:38 +0700

    menu database

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+const menus = [{
+    title: "Avocado Pesto Salad",
+    description: "Home-grown vegetables with load of avocado, topped with mouthwatering pesto salad",
+    photoUrl: "https://www.google.com",
+    prices: [{
+        label: "",
+        price: 35
+    }, {
+        label: "+Grilled Salmon",
+        price: 55
+    }]
+}, {
+    title: "Triple Cheese Pizza",
+    description: "Indulge yourself in this simple yet delicious delicacy",
+    photoUrl: "https://www.google.com",
+    prices: [{
+        label: "1 Slice",
+        price: 15
+    }, {
+        label: "Pan (6 slices)",
+        price: 60
+    }]
+}, {
+    title: "Seasonal Wine",
+    description: "A great meal becomes perfect when combined with a good wine",
+    photoUrl: "https://www.google.com",
+    prices: [{
+        label: "Glass",
+        price: 100
+    }, {
+        label: "Bottle",
+        price: 1500
+    }]
+}, {
+    title: "Coffee",
+    description: "The best coffee you can have from the land of Indonesia",
+    photoUrl: "https://www.google.com",
+    prices: [{
+        label: "Americano",
+        price: 25
+    }, {
+        label: "Latte",
+        price: 30
+    }]
+}];
+
 let qty = 0;
 let qty2 = 0;
 
