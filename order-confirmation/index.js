c60adaa18dc2be81f6ecd273b3408c82809257fa
Moses <moseskarunia@gmail.com>
2025-05-15 16:00:14 +0700

    display price label

--- order-confirmation/index.js
+++ order-confirmation/index.js
@@ -9,8 +9,12 @@ content += `<div>Terimakasih atas pesanan anda: Rp ${(totalPrice * 1000).toLocal
 for (let i = 0; i < cart.length; i++) {
     const e = cart[i];
 
-    if (e[0] > 0 || e[1] > 0) {
-        content += `<div>${menus[i].title}: ${e}</div>`;
+    if (e[0] > 0) {
+        content += `<div>${menus[i].title} (${menus[i].prices[0].label}): ${e[0]}</div>`;
+    }
+
+    if (e[1] > 0) {
+        content += `<div>${menus[i].title} (${menus[i].prices[1].label}): ${e[1]}</div>`;
     }
 }
 
