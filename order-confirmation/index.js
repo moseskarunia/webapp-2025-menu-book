1cc9a2c66152a2c3c27c8357547b2c8f9ab50b40
Moses <moseskarunia@gmail.com>
2025-05-15 15:33:43 +0700

    display only menu with qty > 0

--- order-confirmation/index.js
+++ order-confirmation/index.js
@@ -5,6 +5,13 @@ const cart = JSON.parse(params.get('cart'));
 let content = '';
 
 content += `<div>Terimakasih atas pesanan anda: Rp ${(totalPrice * 1000).toLocaleString()}</div>`;
-content += `<div>Cart = ${cart}`;
+
+for (let i = 0; i < cart.length; i++) {
+    const e = cart[i];
+
+    if (e[0] > 0 || e[1] > 0) {
+        content += `<div>Menu ${i + 1}: ${e}</div>`;
+    }
+}
 
 document.getElementById('outer').innerHTML = content;
